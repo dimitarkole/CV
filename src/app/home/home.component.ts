42004902e022ec9b4cdd06f7521403749bfff5da
Димитър Колев <dim_kolev2002@abv.bg>
2023-08-03 14:28:52 +0300

    remove getIpAddress

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,18 +9,9 @@ import { IpService } from '../core/ip-service.service';
 export class HomeComponent implements OnInit {
 
   constructor(private ipService: IpService) {
-    this.saveIpAddress();
-  }
-
-  saveIpAddress(): void {
-    
-    this.ipService.getIpAddress()
-      .then(ipAddress => {  this.ipService.saveIpAddressToFile(ipAddress) })
-      .catch(error => console.error('Error occurred while fetching the IP address:', error));
   }
 
   ngOnInit(): void {
-    this.saveIpAddress();
   }
 
 }
