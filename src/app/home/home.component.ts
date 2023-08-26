809e2104d9adb4587dce9b19f89bc23277e05540
Димитър Колев <dim_kolev2002@abv.bg>
2023-08-26 23:51:33 +0300

    Add meta data

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IpService } from '../core/ip-service.service';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
@@ -8,10 +9,15 @@ import { IpService } from '../core/ip-service.service';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private ipService: IpService) {
+  constructor(private meta: Meta, private title: Title) {
   }
 
   ngOnInit(): void {
+    this.meta.addTags([
+      { name: 'description', content: 'Dimitar Kolev .net developer portfolio web site' },
+      { name: 'author', content: 'Dimitar Kolev' },
+      { name: 'keywords', content: 'Димитър Колев, Dimitar, Kolev, Dimitar Kolev, Angular, .Net, c# developer, Dimitar Kolev portfolio, developer, програмист' }
+    ]);
   }
 
 }
