dd2afbb4357bae9115a131abacb319f60c8af3d1
Димитър Колев <dim_kolev2002@abv.bg>
2023-11-14 18:28:41 +0200

    Auto-generated commit [ci skip]

--- scripts.6dc99ff3c9691292.js
+++
@@ -1,1 +0,0 @@
-function initMap(){var a={lat:-6.20769,lng:106.98527},e=new google.maps.Map(document.getElementById("google-maps"),{zoom:17,center:a});new google.maps.Marker({position:a,map:e,title:"South Jakarta, INA"})}
